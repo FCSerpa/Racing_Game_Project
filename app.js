7377c15eeba683ad095cf85da4396f21d0225656
Francis Serpa <fcserpa@gmail.com>
2015-08-23 23:45:57 -0700

    end works -button/s

--- app.js
+++ app.js
@@ -1,59 +1,3 @@
-// OOP Racing Game example boilerplate code
-
-function Game(time) {
-  //Create a new instance of player 1
-  //this.player1 = ...
-
-  //Do the same for a player 2
-  //this.player2 = ...
-
-  //Create the track
-  //this.track = ...
-  this.startTime = time;
-
-};
-
-// `Game.prototype.init` kicks off a new game with a board and two players
-Game.prototype.init = function() {
-  // Remove choice screen. Place avatars. Place effects (fish?)? Ready/Set/Go?
-  //Set player2 name === 'computer'(change case) to automate player2?
-  var player1 = new Player($("#player1Name").val(), $('#player1Color option:selected').val(), 'up', 87, 83, 65, 68);
-  var player2 = new Player($("#player2Name").val(), $('#player2Color option:selected').val(), 'down', 73, 75, 74, 76);
-  $("div#t0up").append(player1.imgR);
-  $("div#t0down").append(player2.imgR);
-  $("div#formDiv").empty();
-  $("div#formDiv").prepend('<h1 style="font-size: 10em">READY!</h1>');
-  readySetGo('SET!', 1000);
-  readySetGo('GO!!!', 2000);
-  setTimeout(function(){
-    $("div#formDiv").hide();
-    player1.move(player1);
-    player2.move(player2);
-  }, 3000);
-  
-};
-function readySetGo(rsg, timer){
-  setTimeout(function(){
-    $("h1").text(rsg);
-}, timer);
-};
-function keyboardOn(){
-//of Game?
-// functions for each button? In terms of player?
-};
-function keyboardOff(){
-//of Game?
-// functions for each button? In terms of player?
-};
-Game.prototype.end = function() {
-  // Stop game. Show victory screen. Score? Time?
-};
-Game.prototype.playAgain = function() {
-  // Redundant? Can this be built into init?
-};
-Game.prototype.restart = function() {
-  // Back to choices screen.
-};
 // A starter Player constructor.
 function Player(name, color, side, u, d, l, r) {
   //this.name = ...
@@ -71,11 +15,33 @@ function Player(name, color, side, u, d, l, r) {
   //wins?
 };
 
+function init() {
+  // Remove choice screen. Place avatars. Place effects (fish?)? Ready/Set/Go?
+  //Set player2 name === 'computer'(change case) to automate player2?
+  player1 = new Player($("#player1Name").val(), $('#player1Color option:selected').val(), 'up', 87, 83, 65, 68);
+  player2 = new Player($("#player2Name").val(), $('#player2Color option:selected').val(), 'down', 73, 75, 74, 76);
+  player1.name = player1.name || "Player One";
+  player2.name = player2.name || "Player Two";
+  $("div#t0" + player1.side).append(player1.imgR);
+  $("div#t0" + player2.side).append(player2.imgR);
+  readySetGo('READY!', 0);
+  readySetGo('SET!', 1000);
+  readySetGo('GO!!!', 2000);
+  setTimeout(function(){
+    $("div#formDiv").hide();
+    $("div#formDiv").empty();
+    var time = Date.now();
+    player1.move(player1, time);
+    player2.move(player2, time);
+  }, 3000); 
+
+};
+
 // Remember: prototypes are shared functions between all game instances
-Player.prototype.move = function(player) {
+Player.prototype.move = function(player, time) {
   //update player's position
   $(window).keydown(function(e) {
-    // d movement- Player One right
+    // movement- right
     if (event.keyCode === player.r) {
       if ((player.progress < 6) || ((player.progress > 13) && (player.progress < 19))){
         $("div#t" + player.progress.toString() + player.side).empty();
@@ -89,10 +55,10 @@ Player.prototype.move = function(player) {
         $("div#t" + player.progress.toString() + player.side).empty();
         $("div#t" + (player.progress + 1) + player.side).append(player.imgR);
         player.progress += 1;
-        window.alert("Congratulations!  " + player.name + " wins!");
-        reset();
+        end(player, time);
+
       }
-    // s movement- Player One down
+    // movement- down
     }else if (event.keyCode === player.d){
       if (player.progress === 6){
         $("div#t" + player.progress.toString() + player.side).empty();
@@ -103,7 +69,7 @@ Player.prototype.move = function(player) {
         $("div#t14" + player.side).append(player.imgR);
         player.progress += 1;
       }
-    // a movement- Player One left
+    // movement- left
     }else if (event.keyCode === player.l) {
       if (((player.progress > 0) && (player.progress < 7)) || (player.progress > 14)){
         $("div#t" + player.progress.toString() + player.side).empty();
@@ -114,7 +80,7 @@ Player.prototype.move = function(player) {
         $("div#t" + (player.progress + 1) + player.side).append(player.imgL);
         player.progress += 1;
       }
-    // w movement- Player One up
+    // movement- up
     }else if (event.keyCode === player.u){
       if (player.progress === 7){
         $("div#t" + player.progress.toString() + player.side).empty();
@@ -129,21 +95,39 @@ Player.prototype.move = function(player) {
   });
 };
 
-
-// A starter Track constructor.
-function Track() {
-  //Tracks the cells of the board instance
-  //this.$cells = ...
-
-  //Store any other properties that board may have below, such as a reset option
+function readySetGo(rsg, timer){
+  setTimeout(function(){
+    $("div#formDiv").empty();
+    $("div#formDiv").append('<h1 style="font-size: 10em">' + rsg + '!</h1>');
+}, timer);
 };
 
+function end(player, time) {
+  // Stop game. Show victory screen. Score? Time?
+  console.log(player);
+  console.log(player.name);
+  console.log(time);
+  $("div#formDiv").show();
+  $("div#formDiv").append("<br>" + player.imgR);
+  $("div#formDiv").append("<h1>" + player.name + " wins!</h1><br><h1>" + (Date.now() - time)/1000 + " seconds!</h1>");
+
+};
+
+function playAgain() {
+  // Redundant? Can this be built into init?
+};
+
+function restart() {
+  // Back to choices screen.
+};
+
+
+
+
 $( document ).ready(function(){
-// Start the game!
-  var game = new Game();
 
   $("#nameColorBtns").click(function(){
-    game.init();
+    init();
   });
 
 
